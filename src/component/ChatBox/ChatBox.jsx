43a78a413d527e217ab408cd0d70404869a16b56
adityasharma68 <mr.aditya9675@gmail.com>
2025-05-04 13:33:47 +0530

    enhance chat functionality with message display and styling updates

--- src/component/ChatBox/ChatBox.jsx
+++ src/component/ChatBox/ChatBox.jsx
@@ -10,6 +10,41 @@ const ChatBox = () => {
                 <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
                 <img src={assets.help_icon} className='help' alt="" />   
             </div>  
+
+
+
+            <div className="chat-message">
+                <div className="s-message">
+                    <p className="message">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum amet aut pariatur nam autem quo voluptatum earum asperiores consequuntur deleniti?
+                    </p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+                
+                <div className="s-message">
+                    <img className='message-img' src={assets.pic1} alt="" />
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+
+                <div className="r-message">
+                    <p className="message">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum amet aut pariatur nam autem quo voluptatum earum asperiores consequuntur deleniti?
+                    </p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+            </div>
+
+
+
             <div className="chat-input">
                 <input type="text" placeholder='Send a Message' name="" id="" />
                 <input type="file" accept='image/png, image/jpeg' hidden id='image' />  
